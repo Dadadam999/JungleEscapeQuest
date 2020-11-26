51190997e5b8a5dad4e30760c7ba3fdcd74b7a90
Alexey <alexnes.v1@outlook.com>
2020-11-26 06:02:13 +0300

    добавил анимики пермещения перса

--- player/player.go
+++ player/player.go
@@ -16,8 +16,8 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/player/player.tilesource\"\n"
-  "default_animation: \"up-idle\"\n"
+  data: "tile_set: \"/player/playerRUN.tilesource\"\n"
+  "default_animation: \"down\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
