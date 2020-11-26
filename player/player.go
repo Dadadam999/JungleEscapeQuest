79854bcdc1ef1a64fa689481168bfe1b2fbdc31a
Dadadam999 <Antilamer94@gmail.com>
2020-11-27 00:37:36 +0200

    Moving animation and restructure

--- player/player.go
+++ player/player.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/player/player.tilesource\"\n"
-  "default_animation: \"down-idle\"\n"
+  "default_animation: \"down\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
