62d4058e963ec4ae279b358704a2d97233b20797
Alexey <alexnes.v1@outlook.com>
2020-11-27 11:58:27 +0300

    хтанически зафлексил

--- items/lvl2_object2.go
+++ items/lvl2_object2.go
@@ -45,6 +45,35 @@ embedded_components {
   "    index: 0\n"
   "    count: 1\n"
   "  }\n"
+  "  data: 25.1895\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject1"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"lvl2obj2\"\n"
+  "mask: \"hero\"\n"
+  "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
@@ -58,10 +87,9 @@ embedded_components {
   "      z: 0.0\n"
   "      w: 1.0\n"
   "    }\n"
-  "    index: 1\n"
+  "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 25.1895\n"
   "  data: 21.6945\n"
   "  data: 8.6025\n"
   "  data: 8.7585\n"
